1393c762ecdc72f13c8886c7d7519ac2827f40bd
Aseel Alshohatee <46002084+Alshohatee@users.noreply.github.com>
2022-01-16 21:22:54 -0500

    update the code to instead of required I used import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-let React = require("react");
-let ReactDOM = require("react-dom");
+// let React = require("react");
+// let ReactDOM = require("react-dom");
+import react from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+ReactDOM.render(
+  // only one element to render
+  <div>
+    <h1>Hello World!</h1>
+    <p> This is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
